98cc3d45b9bdb9de434be9fc68f239e01cb47f7a
Hardik Sharma <160159942+hardiksh28@users.noreply.github.com>
2025-07-20 17:34:29 +0530

    Add files via upload

---
+++ Web dev Week -1/ass4.js
@@ -0,0 +1,7 @@
+function sum(a){
+    for(let i=1;i<a;i++){
+        console.log(i+a)
+    }
+}
+let ans = sum(5);
+// console.log(ans);
---
+++ Web dev Week -1/ass5.js
@@ -0,0 +1,9 @@
+function greet(user){
+    console.log("Khamaghandi "+ user.name +" your age is "+ user.age);
+}
+let user = {
+    name: "Hardik",
+    age: 20,
+}
+
+greet(user);
